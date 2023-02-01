a4fff0eca35c9cae6a09fb4df6b442ad53300e15
userzahar <110730909+userzahar@users.noreply.github.com>
2023-02-01 16:12:50 +0200

    трохи стилізую

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import './css/styles.css';
+const axios = require('axios').default;
+const API_KEY = '33289628-97fffc14136600725dd3f07c9';
+const searchForm = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
 
-const DEBOUNCE_DELAY = 300;
+searchForm.addEventListener('submit', onSubmitClick);
+
+async function onSubmitClick(e) {
+  const arrObj = await getSearchArr(getValueForm(e));
+
+  console.log(arrObj);
+  gallery.innerHTML = await renderCard(arrObj);
+}
+
+function getValueForm(e) {
+  e.preventDefault();
+  let getValue = e.target.elements.searchQuery.value;
+  return getValue;
+}
+
+async function getSearchArr(text) {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${text}&image_type=photo&orientation=horizontal&safesearch=true`
+    );
+    return await response.data.hits;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function renderCard(arr) {
+  return await arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes:${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views:${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments:${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads:${downloads}</b>
+          </p>
+        </div>
+      </div>`;
+      }
+    )
+    .join('');
+}
